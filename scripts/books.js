ffaade4950bdda36ddc57b27d1444fc7ae1783d9
Mohammad Akbary <mohammad_aky@yahoo.com>
2025-09-25 16:55:42 +0330

    borrow books script

---
+++ scripts/books.js
@@ -0,0 +1,28 @@
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
+async function fetchCurrentUser(token) {
+  try {
+    const response = await fetch("https://karyar-library-management-system.liara.run/api/auth/me", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+const authToken = getCookie("token");
+fetchCurrentUser(authToken).then((response) => {
+  //   document.getElementsByClassName("").innerText = response.data.user.firstName;
+  document.querySelector(".user-avatar").innerText = response.data.user.firstName[0];
+  document.querySelector(".user-avatar").nextElementSibling.innerText =
+    response.data.user.firstName;
+});
